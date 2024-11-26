cad77ea6752a58c78261e9b073abfce23dbd0527
Guillome <guillome.mignon@gmail.com>
2024-11-26 16:55:01 +0100

    Delete pdo.php

--- pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=mini_blog;charset=utf8', 'root', 'admin');
-} catch (Exception $e) {
-    echo '<script>console.warn("' . $e->getMessage() . '")</script>';
-}
-?>
